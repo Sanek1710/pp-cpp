0e45772352bb217512850c05e56deba67be0bee4
Sanek1710 <sirota.al@inbox.ru>
2025-03-04 19:43:45 +0300

    better token parse order

--- Cursor.cpp
+++ Cursor.cpp
@@ -113,172 +113,112 @@ void Preprocessor::skip_include_string() {
   if (*cur.it != '\n') ++cur.it;
 }
 
-deadnote(int, nother);
-deadnote(int, nspace);
-deadnote(int, nword);
-deadnote(int, nidentifiers);
-deadnote(int, nnums);
-deadnote(int, nslash);
-deadnote(int, nhash);
-deadnote(int, ndot);
-deadnote(int, nstring);
-deadnote(int, nbackslash);
-deadnote(int, nchecks);
-
-deadnote(int, npunct);
-
-struct token_id {
-  // basically character mapping to enum like constants:
-  // any space -> ' ', except from '\n'
-  // any number -> '0'
-  // any identifier -> 'a'
-  // line comment -> 'c'
-  // multiline comment -> 'm'
-  // string -> '"'
-  // all the others maps to themselves
-  // potentially allows to build string of tokens
-  // and apply some pattern matching
-  // e.g.:
-  // `MACRO(arg, arg)` -> `a(a, a)`
-  // `int var = 5` -> `a a = 0`
-  // `const char* str = "string"` -> `a a* a = "`
-  static constexpr char space = ' ';
-  static constexpr char newline = '\n';
-
-  static constexpr char number = '0';
-  static constexpr char identifier = 'a';
-
-  static constexpr char line_comment = 'c';
-  static constexpr char multiline_comment = 'm';
-
-  static constexpr char string = '"';
-};
-
-template <bool pp_line>
-void Preprocessor::process_token() {
-  // ++getnote(nchecks);
+template <bool ppline>
+token_id Preprocessor::tokenise_next() {
   if (is_space(*cur.it)) { /*0*/
-    // ++getnote(nspace);
-    // register_tkind(space);
     do {
       if (*cur.it == '\n') {
         cur.enter();
         cur.clear_line = true;
-        if (pp_line) {
+        if (ppline) {
           ++cur.it;
-          return /*newline*/;
+          return token::newline;
         }
       }
     } while (++cur.it, cur.it != end && is_space(*cur.it));
-    return /* space */;
+    return token::space;
   }
 
-  // ++getnote(nchecks);
   if (std::isalnum(*cur.it) || *cur.it == '_') { /*3*/
-    // ++getnote(nword);
     cur.clear_line = false;
     if (std::isdigit(*cur.it)) {
-      // ++getnote(nnums);
-      // register_tkind(nums);
       skip_number();
-      return /* number */;
+      return token::number;
     }
-    // ++getnote(nidentifiers);
-    // register_tkind(identifiers);
     const iterator start = cur.it;
     skip_identifier();
-    size_t tokan_size = cur.it - start;
-    if (cur.it == end) return /* identifier */;
-    if (cur.it != end  //
-        && (*cur.it == '\'' || *cur.it == '"') &&
-        is_string_prefix(std::string_view{start, tokan_size})) {
-      skip_string_literal(pp_line);
-      return;
+    const size_t tokan_size = cur.it - start;
+    if (cur.it != end                           //
+        && (*cur.it == '\'' || *cur.it == '"')  //
+        && is_string_prefix(std::string_view{start, tokan_size})) {
+      skip_string_literal(ppline);
+      return token::string;
     }
-    // process_identifier
-    return;
+    return token::identifier;
   }
 
-  // ++getnote(nchecks);
   if (*cur.it == '/') { /*2*/
-    // ++getnote(nslash);
-    // register_tkind(slash);
-    if (std::next(cur.it) == end) {
-      ++cur.it;
-      return;
-    }
-    if (*std::next(cur.it) == '/') {
-      skip_line_comment();
-      // process_line_comment
-      return;
-    } else if (*std::next(cur.it) == '*') {
-      skip_multiline_comment();
-      // process_multiline_comment
-      return;
+    if (std::next(cur.it) != end) {
+      if (*std::next(cur.it) == '/') {
+        skip_line_comment();
+        return token::line_comment;
+      }
+      if (*std::next(cur.it) == '*') {
+        skip_multiline_comment();
+        return token::multiline_comment;
+      }
     }
     cur.clear_line = false;
     ++cur.it;
-    return;
+    return '/';
   }
 
-  // ++getnote(nchecks);
   if (*cur.it == '\'' || *cur.it == '"') { /*4*/
-    // ++getnote(nstring);
-    // register_tkind(string);
     cur.clear_line = false;
-    skip_string_literal(pp_line);
-    return;
+    skip_string_literal(ppline);
+    return token::string;
   }
 
-  // ++getnote(nchecks);
   if (*cur.it == '\\') { /*1*/
-    // ++getnote(nbackslash);
-    // register_tkind(backslash);
     ++cur.it;
     if (cur.it != end && *cur.it == '\n') {
       cur.enter();
       ++cur.it;
-      return /* line_continuation */;
+      return token::line_continuation;
     }
-    return /*'\\'*/;
+    return '\\';
   }
 
-  // if (*cur.it == '#') { /*5*/
-  //   ++getnote(nhash);
-  //   if (cur.clear_line) {
-  //     // process directive
-  //   }
-  //   ++cur.it;
-  //   return;
-  // }
+  if (*cur.it == '#') { /*5*/
+    ++cur.it;
+    if constexpr (ppline) {
+      if (cur.it != end && *cur.it == '#') {
+        ++cur.it;
+        return token::pp_op_cat;
+      }
+      return token::pp_op_str;
+    } else {
+      if (cur.clear_line) return token::pp_start;
+      return '#';
+    }
+  }
 
-  // if (*cur.it == '.') { /*6*/
-  //   ++getnote(ndot);
-  //   cur.clear_line = false;
-  //   ++cur.it;
-  //   if (end - cur.it >= 2  //
-  //       && *cur.it == '.' && *std::next(cur.it) == '.') {
-  //     ++ ++cur.it;
-  //     return /* ellipsis */;
-  //   }
-  //   return /* '.' */;
-  // }
+  // we are not really interested in ellipsis if its outta ppline
+  if constexpr (ppline) {
+    if (*cur.it == '.') { /*6*/
+      ++cur.it;
+      if (end - cur.it >= 2  //
+          && *cur.it == '.'  //
+          && *std::next(cur.it) == '.') {
+        ++ ++cur.it;
+        return token::ellipsis;
+      }
+      return '.';
+    }
+  }
 
+  // others
   /*7*/
-  // getnote(others).insert(*cur.it);
-  // ++getnote(nchecks);
-  // register_tkind(other);
-  // ++getnote(nother);
   ++cur.it;
-  return /* *cur.it */;
+  return *cur.it;
 }
 
 int main(int argc, char* argv[]) {
   timeit;
   checkin;
   //~8.9 Mb
-  // std::string src = read_file(ROOT "/pp.test/test.cpp");
-  std::string src = read_file(ROOT "/sqliteall.c");
+  std::string src = read_file(ROOT "/pp.test/test.cpp");
+  // std::string src = read_file(ROOT "/sqliteall.c");
   printit(src.size());
   std::string out;
 
@@ -289,7 +229,7 @@ int main(int argc, char* argv[]) {
     // printit(it.nleft());
   }
   write_file(ROOT "/out.pp.c", out);
-  // return true;
+  return true;
   size_t summer = 0;
 
   if (true) {
--- Cursor.h
+++ Cursor.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <iomanip>
+#include <iostream>
 #include <string_view>
+
+#include "ankerl/unordered_dense.h"
+#include "helper.h"
 
 struct Position {
   unsigned line;
@@ -27,14 +32,91 @@ struct Cursor {
   }
 };
 
+using token_id = char;
+struct token {
+  // basically character mapping to enum like constants:
+  // any space -> ' ', except from '\n'
+  // any number -> '0'
+  // any identifier -> 'a'
+  // line comment -> 'c'
+  // multiline comment -> 'm'
+  // string -> '"'
+  // all the others maps to themselves
+  // potentially allows to build string of tokens
+  // and apply some pattern matching
+  // e.g.:
+  // `MACRO(arg, arg)` -> `a(a, a)`
+  // `int var = 5` -> `a a = 0`
+  // `const char* str = "string"` -> `a a* a = "`
+  static constexpr token_id eof = '\0';
+
+  static constexpr token_id space = ' ';
+  static constexpr token_id newline = '\n';
+
+  static constexpr token_id number = '0';
+  static constexpr token_id identifier = 'a';
+
+  static constexpr token_id line_comment = 'c';
+  static constexpr token_id multiline_comment = 'm';
+
+  static constexpr token_id string = '"';
+
+  static constexpr token_id pp_start = 'p';
+  static constexpr token_id pp_op_str = '1';
+  static constexpr token_id pp_op_cat = '2';
+  static constexpr token_id ellipsis = 'e';
+
+  static constexpr token_id line_continuation = 'z';
+};
+
+struct Token {
+  using iterator = std::string_view::iterator;
+  iterator start;
+  iterator end;
+  Position pos;
+  token_id id;
+
+  std::string_view get_text() const {
+    return {start, static_cast<size_t>(end - start)};
+  }
+
+  void print(std::ostream& os) const {
+    os << "[" << std::setw(3) << pos.line    //
+       << ":" << std::setw(2) << pos.column  //
+       << "]: `" << ctrl_str{get_text()} << "`\n";
+  }
+};
+
 class Preprocessor {
  public:
   Preprocessor(std::string_view src)
       : src{src}, cur{src.begin()}, end{src.end()} {}
 
+  ankerl::unordered_dense::set<std::string_view> identifiers;
+
   void process_code() {
     while (cur.it < end) {
-      process_token<false>();
+      read_token();
+      // token.print(std::cerr);
+      if (token.id == token::eof) break;
+      if (token.id == token::identifier) {
+        if (auto it = identifiers.find(token.get_text());
+            it != identifiers.end()) {
+          std::cout << token.get_text();
+        }
+        // process identifier
+        continue;
+      }
+      if (token.id == token::pp_start) {
+        token.print(std::cerr);
+        read_token(true);
+        token.print(std::cerr);
+        read_token(true);
+        token.print(std::cerr);
+        // std::cerr << "#\n";
+        // process ppstart
+        continue;
+      }
     }
   }
 
@@ -42,6 +124,8 @@ class Preprocessor {
   std::string_view src;
   Cursor cur;
   Cursor::iterator end;
+
+  Token token;
 
   // skip basic tokens assuming being on their start
   inline void skip_identifier();
@@ -53,6 +137,50 @@ class Preprocessor {
   // skip special tokens
   inline void skip_include_string();
 
-  template <bool pp_line>
-  void process_token();
+  template <bool ppline>
+  token_id tokenise_next();
+
+  template <bool ppline>
+  inline bool is_extra(token_id token) {
+    return token == token::space                 //
+           || token == token::multiline_comment  //
+           || token == token::line_comment       //
+           || token == token::line_continuation  //
+           || (!ppline && token == token::newline);
+  }
+
+  template <bool ppline>
+  void read_token_template(bool skip_extras) {
+    while (cur.it != end) {
+      token.start = cur.it;
+      token.pos = cur.to_position();
+      token.id = tokenise_next<ppline>();
+      token.end = cur.it;
+      if (!(skip_extras && is_extra<ppline>(token.id))) return;
+    }
+    set_eof();
+    return;
+  }
+  void read_token(bool skip_extras = false) {
+    read_token_template<false>(skip_extras);
+  }
+  void read_pptoken(bool skip_extras = false) {
+    read_token_template<true>(skip_extras);
+  }
+
+  bool consume_token(token_id id, bool skip_extras = false) {
+    if (token.id != id) return false;
+    read_token(skip_extras);
+  }
+  bool consume_pptoken(token_id id, bool skip_extras = false) {
+    if (token.id != id) return false;
+    read_token(skip_extras);
+  }
+
+  void set_eof() {
+    token.start = cur.it;
+    token.pos = cur.to_position();
+    token.id = token::eof;
+    token.end = cur.it;
+  }
 };
