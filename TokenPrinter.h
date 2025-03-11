e7295460938e24aa84f48b2385230f826815d0be
Sanek1710 <sirota.al@inbox.ru>
2025-03-11 23:53:29 +0300

    forgot token printer

--- TokenPrinter.h
+++ TokenPrinter.h
@@ -4,13 +4,29 @@
 
 #include "Cursor.h"
 
+
+constexpr uint32_t mask3(Cursor::iterator it, Cursor::iterator end) {
+  uint32_t val = 0;
+  for (int i = 0; i < 4 && it != end; ++i, ++it) {
+    val = (val << 8) | *it;
+    if (*it == '"' || *it == '\'') return val;
+  }
+  return 0;
+}
+constexpr uint32_t mask3(std::string_view sv) {
+  return mask3(sv.begin(), sv.end());
+}
+
+constexpr auto m1 = mask3("u\"1231231213213");
+constexpr auto m2 = mask3("u\"12312sdvsdv13");
+
 class TokenPrinter {
  public:
   TokenPrinter(std::ostream& os) : os(os) {}
 
-  inline void print(Token token) {
-    std::string_view text = token.get_text();
-    auto clr = [text, &token]() -> const char* {
+  inline void print(Token token, std::string_view src) {
+    std::string_view text = token.get_text(src);
+    auto clr = [text, &token, &src]() -> const char* {
       std::unordered_set<std::string_view> keywords = {
           "return",  "if",     "using", "while", "do",    "break",
           "else",    "for",    "#",     "ifdef", "endif", "else",
@@ -25,26 +41,27 @@ class TokenPrinter {
           "std",     "string",   "size_t", "string_view", "unordered_set",
           "ostream", "iterator", "auto",   "uint32_t",    "vector"};
       if (text.empty()) return "";
-      if (token.id == token::line_comment ||
-          token.id == token::multiline_comment)
+      if (token.id == Token::line_comment ||
+          token.id == Token::multiline_comment)
         return "\033[38;5;22m";
-      if (token.id == token::string_like_literal) return "\033[38;5;216m";
+      if (token.id == Token::string_like_literal) return "\033[38;5;216m";
       if (keywords.count(text)) return "\033[38;5;176m";
       if (keywords2.count(text)) return "\033[38;5;75m";
       if (keywords3.count(text)) return "\033[38;5;37m";
       if (std::isdigit(text.front())) return "\033[38;5;193m";
       if (text.front() == '(' || text.front() == ')') return "\033[38;5;228m";
       if (std::ispunct(text.front())) return "\033[38;5;248m";
-      if (token.id == token::identifier && *token.end == ':')
+      if (token.id == Token::identifier && src[token.range.end] == ':')
         return "\033[38;5;37m";
-      if (token.id == token::identifier && *token.end == '(')
+      if (token.id == Token::identifier && src[token.range.end] == '(')
         return "\033[38;5;230m";
 
       return "\033[38;5;153m";
     };
-    if (last_line != token.pos.line) {
-      last_line = token.pos.line;
-      os << "\033[38;5;240m" << std::setw(3) << (token.pos.line + 1) << "│ ";
+    if (last_line != token.range.start_pos.line) {
+      last_line = token.range.start_pos.line;
+      os << "\033[38;5;240m" << std::setw(3) << (token.range.start_pos.line + 1)
+         << "│ ";
     }
     os << clr() << text << "\033[0m";
   }
